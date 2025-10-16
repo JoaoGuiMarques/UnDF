f67c6fd8d379421b1f975a8dae0793f1cbb6e218
João Guilherme Marques <joaoguiom29@gmail.com>
2025-10-15 21:40:35 -0300

    Loop adicionado

--- Projeto-Aplicado/escolhaled.c
+++ Projeto-Aplicado/escolhaled.c
@@ -1,53 +1,65 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-int main(void) //inÌcio do programa
+int main(void) //in√≠cio do programa
 {
-    int escolha;  //declara vari·vel escolha, que armazena o n˙mero do LED a ser escolhido
-    int tempo;    //declara vari·vel tempo, armazenando o tempo em segundos
+    int escolha;  //declara vari√°vel escolha, que armazena o n√∫mero do LED a ser escolhido
+    int tempo;    //declara vari√°vel tempo, armazenando o tempo em segundos
 
     wiringPiSetup();            // inicia a biblioteca WiringPi
-    pinMode(1, OUTPUT);         // configura o pino 1 como saÌda
-    pinMode(4, OUTPUT);         // configura o pino 4 como saÌda
-    pinMode(5, OUTPUT);         // configura o pino 5 como saÌda
-
-    printf("Escolha o LED que vocÍ deseja acender:\n");  // imprime mensagem na tela
+    pinMode(1, OUTPUT);         // configura o pino 1 como sa√≠da, f√≠sico 12
+    pinMode(4, OUTPUT);         // configura o pino 4 como sa√≠da, f√≠sico 16
+    pinMode(5, OUTPUT);         // configura o pino 5 como sa√≠da, f√≠sico 18
+	
+	while(1) {
+	
+    printf("Escolha o LED que voc√™ deseja acender ou 0 se voc√™ deseja sair:\n");  // imprime mensagem na tela
     printf("1 - Verde\n");       // imprime mensagem na tela
     printf("2 - Amarelo\n");     // imprime mensagem na tela
     printf("3 - Vermelho\n");    // imprime mensagem na tela
-    scanf("%d", &escolha);       // lÍ o n˙mero digitado pelo usu·rio (%d) e armazena na vari·vel ìescolhaî
+    printf("0 - Sair\n");
+    printf("Digite o n√∫mero: ");
+    scanf("%d", &escolha);       // l√™ o n√∫mero digitado pelo usu√°rio (%d) e armazena na vari√°vel ‚Äúescolha‚Äù
 
+	if (escolha == 0) {
+		printf("Programa encerrado\n");
+		break;
+	}
+	
+	if (escolha < 1 || escolha > 3 ) { // Se a op√ß√£o for inv√°lida (n√£o for 1, 2 ou 3), mostra erro e volta pro menu
+		printf("Op√ß√£o inv√°lida! Digite 1, 2, 3 ou 0 para sair.\n");
+		continue; // volta pro in√≠cio do while
+	}
+		
     printf("Por quantos segundos o LED deve ficar aceso? ");
-    scanf("%d", &tempo);         // lÍ o n˙mero digitado pelo usu·rio (%d) e armazena na vari·vel ìtempoî
+    scanf("%d", &tempo);         // l√™ o n√∫mero digitado pelo usu√°rio (%d) e armazena na vari√°vel ‚Äútempo‚Äù
 
-    switch (escolha) // cria possÌveis casos de acordo com a escolha do usu·rio
+    switch (escolha) // cria poss√≠veis casos de acordo com a escolha do usu√°rio
     {
         case 1:
-            printf("LED verde ficar· aceso por %d segundos...\n", tempo);
+            printf("LED verde ficar√° aceso por %d segundos...\n", tempo);
             digitalWrite(1, HIGH);
             delay(tempo * 1000);  // tempo em segundos convertido para milissegundos
             digitalWrite(1, LOW);
             break;
 
         case 2:
-            printf("LED amarelo ficar· aceso por %d segundos...\n", tempo);
+            printf("LED amarelo ficar√° aceso por %d segundos...\n", tempo);
             digitalWrite(4, HIGH);
             delay(tempo * 1000);
             digitalWrite(4, LOW);
             break;
 
         case 3:
-            printf("LED vermelho ficar· aceso por %d segundos...\n", tempo);
+            printf("LED vermelho ficar√° aceso por %d segundos...\n", tempo);
             digitalWrite(5, HIGH);
             delay(tempo * 1000);
             digitalWrite(5, LOW);
             break;
-
-        default:
-            printf("OpÁ„o inv·lida!\n"); // caso o usu·rio digite um n˙mero diferente de 1, 2 ou 3
     }
 
-    printf("LED apagado. Programa concluÌdo.\n"); // mensagem exibida no final
+    printf("LED apagado...\n"); // mensagem exibida no final
+	}
+	
     return 0;
 }
-
