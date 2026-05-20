9781b114cc932d67f290dbc5770ded61b18b15c0
João Guilherme Marques <joaoguiom29@gmail.com>
2026-05-20 11:03:45 -0300

    herança

---
+++ heranca/Aluno.java
@@ -0,0 +1,32 @@
+package heranca;
+
+public class Aluno extends Pessoa {
+	
+	private String curso;
+	
+	public Aluno(String nome, int idade, String curso) {
+		super(nome, idade);
+		this.curso = curso;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	//métodos específicos
+		@Override
+		public void apresentar() {
+			super.apresentar(); //método presente na superclasse
+			System.out.println("Curso: " + curso);
+		}
+		
+		@Override
+		public String toString() {
+			return super.toString() + ", Curso: " + curso;
+		}
+
+}
